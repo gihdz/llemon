23bb3929b0100b57b867574687570ec6fa5f9c98
Giancarlo <gianhdz89@gmail.com>
2023-07-18 23:43:01 -0400

    Support svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,45 @@
 import React from "react";
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer>
+      <div>
+        <div>Navigation</div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Reservations</a>
+          </li>
+          <li>
+            <a href="/">Order Online</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div>Contact</div>
+        <ul>
+          <li>Address</li>
+          <li>phone number</li>
+          <li>email</li>
+        </ul>
+      </div>
+      <div>
+        <div>Social media links</div>
+        <ul>
+          <li>Address</li>
+          <li>phone number</li>
+          <li>email</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { ReactComponent as LemonLogo } from "../images/Logo.svg";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  return (
+    <>
+      <LemonLogo />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Reservations</a>
+          </li>
+          <li>
+            <a href="/">Order Online</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
