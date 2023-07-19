3c8849f07ea8a504e95ec7a978e4e5ff1b98ac8f
Giancarlo <gianhdz89@gmail.com>
2023-07-18 22:56:37 -0400

    Setting up initial design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.scss";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
@@ -11,7 +11,9 @@ function App() {
       <Header>
         <Nav />
       </Header>
-      <Main>main</Main>
+      <Main>
+        <div className={styles.title}>Little Lemon</div>
+      </Main>
       <Footer></Footer>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { PropsWithChildren } from "react";
-const Header = (props: PropsWithChildren): JSX.Element => {
+const Header = (props: PropsWithChildren) => {
   return <header>{props.children}</header>;
 };
 
